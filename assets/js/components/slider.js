63d23e037764b1c27c9e5a70f2a7a27c72fd6064
GudiksenTim <1000077d2a@mcdmstudent.dk>
2025-09-11 11:09:50 +0200

    gjorde slideren lidt mere clean

--- assets/js/components/slider.js
+++ assets/js/components/slider.js
@@ -1,6 +1,7 @@
 const sliderContainer = document.querySelector(".slider-container");
 
-let heroText = 'Vi stræber efter at skabe en unik shoppingoplevelse <br> hvor elegance møder funktionalitet';
+let heroText =
+  'Vi stræber efter at skabe en unik shoppingoplevelse <br> hvor elegance møder funktionalitet';
 
 let images = [
   "../assets/display/slider/hero_img.jpg",
@@ -8,39 +9,54 @@ let images = [
   "../assets/display/slider/hero_img3.jpg",
 ];
 
-let i = 0
+let i = 0;
 
-const sliderTmpl = (i) => {
-
+const sliderTmpl = (index) => {
   return `
-    <img class="image-in-slider" src="${images[i]}" alt="" />
-
+    <div class="slide fade-in">
+      <img class="image-in-slider" src="${images[index]}" alt="" />
       <div class="hero-text-container">
         <p class="hero-text fatface">
           ${heroText}
         </p>
-      </div>`;
+      </div>
+    </div>`;
 };
 
 const loadSlider = (index) => {
   if (sliderContainer) {
-    sliderContainer.innerHTML = ''
+    // tilføj nyt billede oveni
     sliderContainer.insertAdjacentHTML("beforeend", sliderTmpl(index));
+
+    // find alle slides
+    const slides = sliderContainer.querySelectorAll(".slide");
+
+    if (slides.length > 1) {
+      const prevSlide = slides[0]; // det gamle
+
+      // fade det gamle ud
+      prevSlide.classList.remove("fade-in");
+      prevSlide.classList.add("fade-out");
+
+      // fjern det gamle fra DOM når animationen er færdig
+      prevSlide.addEventListener("animationend", () => {
+        prevSlide.remove();
+      });
+    }
   }
-}
+};
 
 export const renderSlider = () => {
-
-  loadSlider(i)
+  // vis første slide med det samme
+  loadSlider(i);
 
   setInterval(() => {
-
-    if (i == images.length - 1) {
+    if (i === images.length - 1) {
       i = 0;
     } else {
       i++;
     }
-    loadSlider(i)
+    loadSlider(i);
   }, 3000);
-
-}
+};
+  
