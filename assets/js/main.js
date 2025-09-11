a8bbed29f81664ce5c0cf0bdf15b540bbcc974e7
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-09-11 09:29:29 +0200

    js enable

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,11 @@
 import { Recommended } from "./components/recommended.js";
 Recommended();
 
-/* import { renderSlider } from "./components/slider.js";
-renderSlider(); */ //! fejl
+import { renderSlider } from "./components/slider.js";
+renderSlider();
 
-/* import { renderContact } from "./components/contact.js";
-renderContact(); */ //! fejl
+import { renderContact } from "./components/contact.js";
+renderContact();
 
 import { Gallery } from "./components/gallery.js";
 Gallery();
